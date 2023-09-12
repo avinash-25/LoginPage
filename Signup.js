1c21660ddfbb217187825844d490305365dcd384
avinash-25 <avinashranjan268@gmail.com>
2023-09-12 12:36:10 +0530

    Initial commit

---
+++ Signup.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = () => {
+    // Implement your signup logic here
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleSignup}>Signup</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
